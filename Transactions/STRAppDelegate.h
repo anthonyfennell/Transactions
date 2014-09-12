abf035d1a69b207f68479ee7e228226d26e53e0f
Anthony Fennell <fennell4511@anthonys-imac.attlocal.net>
2014-09-11 19:06:28 -0500

    Initial Commit

---
+++ Transactions/STRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  STRAppDelegate.h
+//  Transactions
+//
+//  Created by Anthony Fennell on 9/11/14.
+//  Copyright (c) 2014 Anthony Fennell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface STRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Transactions/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Transactions
+//
+//  Created by Anthony Fennell on 9/11/14.
+//  Copyright (c) 2014 Anthony Fennell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "STRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([STRAppDelegate class]));
+    }
+}
